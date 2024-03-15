fa74acf5b7cea37a0e715d091063ab2a9168ecb9
Robert Aslan <robert.aslan44@gmail.com>
2024-03-15 15:10:55 -0400

    Created Blocks, and updated Colors

--- tetris_project/src/main.cpp
+++ tetris_project/src/main.cpp
@@ -1,5 +1,6 @@
 #include <raylib.h>
 #include "grid.h"
+#include "blocks.cpp"
 
 int main()
 {
@@ -8,14 +9,16 @@ int main()
     SetTargetFPS(60);
 
     Grid grid = Grid();
-    grid.grid[0][0] = 1;
     grid.Print();
+
+    TBlock block = TBlock();
 
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
         ClearBackground(darkBlue);
         grid.Draw();
+        block.Draw();
         EndDrawing();
     }
 
